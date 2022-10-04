370d47b1ff1d2e99167f99e7d6fbc5fdb365853e
mridulabk <mridula.prabhakar8@gmail.com>
2022-10-04 10:19:19 +0530

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: 'SS Cricket Bat',
+    image: '/images/SS_Cricket_Bat.jpg',
+    description:
+      'The bat is generally designed to hit the ball from its centre blade; this is why the toe of the bat contains less wood. Body Features - SS industries manufacture their bats according to top standards with great, lightweight body shape, amazing pick up and thick, contoured concave edges.',
+    brand: 'SS',
+    category: 'Cricket',
+    price: 6000,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    name: 'Wilson Tennis Racket',
+    image: '/images/Wilson_Racket.jpg',
+    description:
+      'As with all the best Wilson tennis racquets, it is perfectly balanced and very easy to play with. ... The control this racquet gives you is superb, making it simple to put the ball exactly where you want too. Many intermediate and advanced players use this racquet.',
+    brand: 'Wilson',
+    category: 'Tennis',
+    price: 5000 ,
+    countInStock: 10,
+    rating: 4.0,
+    numReviews: 4,
+  },
+  {
+    name: 'Nivia Football',
+    image: '/images/Nivia_Football.jpg',
+    description:
+      'Nivia Sports was the first Indian sports equipment manufacturer to produce hand stitched balls, including footballs. ... Its football became the official ball for the National Football Championship and for the All India Football Federation (AIFF).',
+    brand: 'Nivia',
+    category: 'Football',
+    price: 670,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 3,
+  },
+  {
+    name: 'Shin pads',
+    image: '/images/Shin_Pads.jpg',
+    description:
+      'A shin guard or shin pad, is a piece of equipment worn on the front of an athlete shin to protect it from injury. These are commonly used in sports including association football, baseball, ice hockey, field hockey, lacrosse, cricket, mountain bike trials, and other sports.',
+    brand: 'Mivo',
+    category: 'Football',
+    price: 100,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 3,
+  },
+  {
+    name: 'SS Cricket Kit',
+    image: '/images/SS_Cricket_Kit.jpg',
+    description:
+      'Buy SS Cricket Bats, Balls, Kit Bags, Helmet, Sports Wear & Accessories online at best price in India.',
+    brand: 'SS',
+    category: 'Cricket',
+    price: 9999,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 2,
+  },
+  {
+    name: 'Yonex Badminton',
+    image: '/images/Yonex_Badminton.jpg',
+    description:
+      'Yonex Nanoray Light 8i LCW Graphite Badminton Racquet with free Full Cover (Purple/Blue, G4, 77 Grams, 30 lbs Tension).',
+    brand: 'Yonex',
+    category: 'Badminton',
+    price: 500,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 4,
+  },
+]
+
+export default products
